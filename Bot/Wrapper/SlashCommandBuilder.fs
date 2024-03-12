37661d3a6ec5e4b118c60a1bd4b1bbe3c69db745
Samuel <samuel.young@softwire.com>
2024-03-12 22:44:41 +0000

    make a start at a type safe builder

--- Bot/Wrapper/SlashCommandBuilder.fs
+++ Bot/Wrapper/SlashCommandBuilder.fs
@@ -9,3 +9,35 @@ let withName (name: string) (builder: SlashCommandBuilder): SlashCommandBuilder 
 
 let withDescription (name: string) (builder: SlashCommandBuilder): SlashCommandBuilder =
     builder.WithDescription(name)
+    
+// Foo<string, Foo<string, Done>> f: string -> string -> Client
+
+type ICommandType<'a> =
+    abstract member elem: 'a
+
+type StringCommand(e: string) =
+    interface ICommandType<string> with
+        member this.elem = e
+
+type NumberCommand(e: int) =
+    interface ICommandType<int> with
+        member this.elem = e
+
+let func1<'a, 'b> ((command): (ICommandType<'a>)) (handler: ('a -> string -> unit)) =
+    let result = handler command.elem
+    result "5"
+
+let func2 ((command, command2): (ICommandType<'a> * ICommandType<'b>)) (handler: ('a -> 'b -> string -> unit)) =
+    let result = handler command.elem
+    func1 command2 result
+
+let func3 ((command, command2, command3): (ICommandType<'a> * ICommandType<'b> * ICommandType<'c>)) (handler: ('a -> 'b -> 'c -> string -> unit)) =
+    let result = handler command.elem
+    func2 (command2, command3) result
+
+let command = (StringCommand("5"), NumberCommand(5))
+
+let handler (name: string) (age: int) (context: string) =
+    ()
+    
+func2 command handler
