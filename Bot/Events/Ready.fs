68b25cf68d0c9c273acdc792c15f0026c59649c0
Samuel Young <samuel.young@softwire.com>
2024-03-24 11:50:24 +0000

    bulk overwrite commands on load rather than add

--- Bot/Events/Ready.fs
+++ Bot/Events/Ready.fs
@@ -7,8 +7,12 @@ open Bot.Wrapper.SlashCommandBuilder
 let ready (client: IDiscordClient) (commands: BuiltCommand<obj> list) (): Task = task {
     let! guild = client.GetGuildAsync(543719732100988943UL)
     
-    for command in commands do
-        do! guild.CreateApplicationCommandAsync(command.properties) |> Async.AwaitTask |> Async.Ignore
+    let commandProperties =
+        commands
+        |> List.map (fun command -> command.properties :> ApplicationCommandProperties)
+        |> List.toArray
+    
+    do! guild.BulkOverwriteApplicationCommandsAsync(commandProperties) |> Async.AwaitTask |> Async.Ignore
     
     ()
 }
