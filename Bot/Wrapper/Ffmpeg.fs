7e72cf1ed62986261dde2fe9d8a11857524ca8c7
Samuel Young <samuel.young@softwire.com>
2024-03-27 09:53:28 +0000

    move ffmpeg stuff to its own file

---
+++ Bot/Wrapper/Ffmpeg.fs
@@ -0,0 +1,24 @@
+﻿module Bot.Wrapper.Ffmpeg
+
+open System.Diagnostics
+open Discord.Audio
+open Discord.WebSocket
+
+let playIntoChannel (path: string) (channel: SocketVoiceChannel) = task {
+    let! connection = channel.ConnectAsync()
+    
+    let startInfo = ProcessStartInfo()
+    startInfo.CreateNoWindow <- true
+    startInfo.UseShellExecute <- false
+    startInfo.RedirectStandardOutput <- true
+    startInfo.FileName <- "./downloaded-binaries/ffmpeg.exe"
+    startInfo.WindowStyle <- ProcessWindowStyle.Hidden
+    startInfo.Arguments <- $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1"
+    
+    use voiceProcess = Process.Start(startInfo)
+    use stream = voiceProcess.StandardOutput.BaseStream
+    use voiceStream = connection.CreatePCMStream(AudioApplication.Mixed)
+    do! stream.CopyToAsync(voiceStream)
+    do! voiceProcess.WaitForExitAsync()
+    connection.Dispose()
+}
