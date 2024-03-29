60d189391384b6349e51f4ebf6cb14abaf277033
Samuel Young <samuel.young@softwire.com>
2024-03-29 10:39:41 +0000

    make ffmpeg platform specific

--- Bot/Wrapper/Ffmpeg.fs
+++ Bot/Wrapper/Ffmpeg.fs
@@ -1,24 +1,36 @@
 ﻿module Bot.Wrapper.Ffmpeg
 
 open System.Diagnostics
+open System.Threading.Tasks
 open Discord.Audio
 open Discord.WebSocket
 
-let playIntoChannel (path: string) (channel: SocketVoiceChannel) = task {
+let playIntoChannelWithExecutable (ffmpegPath: string) (path: string) (channel: SocketVoiceChannel) = task {
     let! connection = channel.ConnectAsync()
     
     let startInfo = ProcessStartInfo()
     startInfo.CreateNoWindow <- true
     startInfo.UseShellExecute <- false
     startInfo.RedirectStandardOutput <- true
-    startInfo.FileName <- "./downloaded-binaries/ffmpeg.exe"
+    startInfo.FileName <- ffmpegPath
     startInfo.WindowStyle <- ProcessWindowStyle.Hidden
     startInfo.Arguments <- $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1"
     
     use voiceProcess = Process.Start(startInfo)
     use stream = voiceProcess.StandardOutput.BaseStream
-    use voiceStream = connection.CreatePCMStream(AudioApplication.Mixed)
+    use voiceStream = connection.CreatePCMStream(AudioApplication.Music)
     do! stream.CopyToAsync(voiceStream)
     do! voiceProcess.WaitForExitAsync()
     connection.Dispose()
 }
+
+let playIntoChannelWindows = playIntoChannelWithExecutable "./downloaded-binaries/ffmpeg.exe"
+
+let playIntoChannelLinux = playIntoChannelWithExecutable "./downloaded-binaries/ffmpeg"
+
+let playIntoChannel: string -> SocketVoiceChannel -> Task<unit> =
+    let platform = System.Environment.GetEnvironmentVariable("PLATFORM")
+    match platform with
+    | "windows" -> playIntoChannelWindows
+    | "linux" -> playIntoChannelLinux
+    | _ -> failwith "no platform set"
