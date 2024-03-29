b99b97581a82fdff391a53c43361827853c009fa
Samuel Young <samuel.young@softwire.com>
2024-03-29 18:37:11 +0000

    register in all guilds

--- Bot/Events/Ready.fs
+++ Bot/Events/Ready.fs
@@ -1,20 +1,19 @@
 module Bot.Events.Ready
 
-open System.Diagnostics
 open System.Threading.Tasks
 open Discord
 open Bot.Wrapper.SlashCommandBuilder
-open Discord.Audio
 
 let ready (client: IDiscordClient) (commands: BuiltCommand<obj> list) (): Task = task {
-    let! guild = client.GetGuildAsync(543719732100988943UL)
+    let! guilds = client.GetGuildsAsync()
     
     let commandProperties =
         commands
         |> List.map (fun command -> command.properties :> ApplicationCommandProperties)
         |> List.toArray
     
-    do! guild.BulkOverwriteApplicationCommandsAsync(commandProperties) |> Async.AwaitTask |> Async.Ignore
+    for guild in guilds do
+        do! guild.BulkOverwriteApplicationCommandsAsync(commandProperties) |> Async.AwaitTask |> Async.Ignore
     
     ()
 }
