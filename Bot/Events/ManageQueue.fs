6c3d1e6bc7ef0114e421a63c11419ac1000e178c
Samuel Young <samuel.young@softwire.com>
2024-04-07 21:58:01 +0100

    wrap it in a try

--- Bot/Events/ManageQueue.fs
+++ Bot/Events/ManageQueue.fs
@@ -10,31 +10,33 @@ let joinChannel (channel: SocketVoiceChannel) = task {
     return! channel.ConnectAsync()
 }
 
-let rec queueManager (queue: LockedQueue<QueueItem>) = task {
+let queueManager (queue: LockedQueue<QueueItem>) = task {
     let mutable oldChannel: SocketVoiceChannel = null
     let mutable connection = null
     while true do
-        if getQueueLength queue > 0 then
-            let queueItem = popFromQueue queue
-            
-            // if not in a channel or needs to move
-            if oldChannel = null || queueItem.channel.Id <> oldChannel.Id then
-                // establish new connection
-                let! newConnection = joinChannel queueItem.channel
-                connection <- newConnection
-                ()
-            
-            do! playIntoChannel connection queueItem.path queueItem.channel |> Async.AwaitTask
-            
-            oldChannel <- queueItem.channel
-            
-            // if queue is now empty
-            if getQueueLength queue = 0 then
-                do! Task.Delay(5000)
-                // leave
-                do! connection.StopAsync()
-                connection <- null
-                oldChannel <- null
-            
+        try 
+            if getQueueLength queue > 0 then
+                let queueItem = popFromQueue queue
+                
+                // if not in a channel or needs to move
+                if oldChannel = null || queueItem.channel.Id <> oldChannel.Id then
+                    // establish new connection
+                    let! newConnection = joinChannel queueItem.channel
+                    connection <- newConnection
+                    ()
+                
+                do! playIntoChannel connection queueItem.path queueItem.channel |> Async.AwaitTask
+                
+                oldChannel <- queueItem.channel
+                
+                // if queue is now empty
+                if getQueueLength queue = 0 then
+                    do! Task.Delay(5000)
+                    // leave
+                    do! connection.StopAsync()
+                    connection <- null
+                    oldChannel <- null
+        with
+            _ -> ()
         do! Task.Delay(100)
 }
